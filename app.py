05e400996c17806c52da4c9823a88891dd418ea0
VPetras <vojtechpetrasek@gmail.com>
2020-12-14 21:00:35 +0100

    first commit app.py

---
+++ app.py
@@ -0,0 +1,30 @@
+#!/usr/bin/env python3
+# -*- coding: utf-8 -*-
+
+##################################################
+# DEX-IC Hackathon APP
+# Title: This is script which run on Kivy framework for visualization controlled aplication based on raspberry pi
+# Author: Vojtěch Petrásek 
+# Generated: 14/12/2020 20:55:01
+##################################################
+
+###
+# imports section
+###
+
+import sys
+
+def main():
+    print('Hello World')
+
+
+
+
+if __name__ == '__main__':
+    try:
+        main()
+    except KeyboardInterrupt:
+        pass
+    except Exception as e:
+        print('Exited with error: {}'.format(e))
+        sys.exit(1)
